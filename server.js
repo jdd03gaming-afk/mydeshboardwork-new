6c2e586f0190b38659664a1f0d7015e07ce7f225
joniroy <jdd03gaming@gmail.com>
2025-10-17 12:08:44 +0600

    Update server.js

--- server.js
+++ server.js
@@ -44,3 +44,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
